9b28606564f6cf3fbb939d35a3bbc0cb6a216f7a
raggi <jftucker@gmail.com>
2011-03-08 23:11:24 -0800

    Bump to 0.5.0 and prep changelog

--- lib/async_sinatra.rb
+++ lib/async_sinatra.rb
@@ -2,5 +2,5 @@ require 'sinatra/async'
 # This is just a stub module for documentation and versioning. Please see
 # Sinatra::Async.
 module AsyncSinatra
-  VERSION = '0.4.0'
+  VERSION = '0.5.0'
 end
