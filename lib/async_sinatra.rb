b595d258a701625e0b70b0ef1c0a8cca717fdb34
James Tucker <jftucker@gmail.com>
2016-01-16 14:38:27 -0800

    Bump version for release

--- lib/async_sinatra.rb
+++ lib/async_sinatra.rb
@@ -2,5 +2,5 @@ require 'sinatra/async'
 # This is just a stub module for documentation and versioning. Please see
 # Sinatra::Async.
 module AsyncSinatra
-  VERSION = '1.2.0'
+  VERSION = '1.2.1'
 end
